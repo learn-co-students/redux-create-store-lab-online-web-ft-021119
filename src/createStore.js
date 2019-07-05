d9206168536ed4f4641973f74835d5eb85e55bf0
VBTX <jv.svedaite@gmail.com>
2019-07-05 09:20:09 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,31 @@
 export default function createStore(reducer) {
-  // add your code here
-}
+  let state;
+
+
+  function dispatch(action){
+  	state = reducer(state, action);
+  	render()
+  }
+
+
+    function getState(){
+  	return state;
+  }
+
+   // Don't forget, your initial state for each store 
+   // should be set by the reducer. You'll have to 
+   // dispatch some sort of 'initialize' action to get your state set!
+
+
+   dispatch({ type: '@@INIT' })
+
+
+
+  	return {
+  		dispatch,
+  		getState
+  	};
+  }
 
 function render() {
   const container = document.getElementById('container');
