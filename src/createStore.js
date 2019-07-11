5a4ac89374fddc7fd4cb5a1df97c57ae3faa68d1
AudreyJones <audrey.eliz.jones@gmail.com>
2019-07-10 21:16:47 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,7 +1,41 @@
 export default function createStore(reducer) {
-  // add your code here
+  
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state,action);
+    render();
+  }
+
+  function getState(){
+    return state;
+  }
+
+  return {
+    dispatch,
+    getState
+  }
+}
+
+function changeCount(state={count: 0},action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+
+    default:
+      return state;
+  }
 }
 
 function render() {
-  const container = document.getElementById('container');
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count
 }
+
+let store = createStore(changeCount)
+store.dispatch({type:'@@INIT'})
+
+let button = document.getElementById('button');
+button.addEventListener('click', function() {
+  store.dispatch({type: 'INCREASE_COUNT'})
+})
